ee95296a0ae45c9482d965b0cef88983cfb3437a
MShahryiar <muhammadshahryiar@yahoo.com>
2023-02-20 22:10:32 +0500

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-6xl bg-red-200'>Social Media App</h1>
+    </div>
+  );
+}
+
+export default App;
