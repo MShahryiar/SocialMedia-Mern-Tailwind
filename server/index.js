836e41ef4069ff22723fabd83c8d8d82629d7e55
MShahryiar <muhammadshahryiar@yahoo.com>
2023-02-20 22:17:36 +0500

    Initialized Server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>{
+    app.listen(PORT, ()=>console.log(`Server running on PORT : ${PORT}`))
+
+})
+.catch((error)=> console.log(`${error} did not connect.`))
