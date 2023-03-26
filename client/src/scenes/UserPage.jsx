018a5b21998a8c994996cd1f306e4d5f2ebb5360
MShahryiar <muhammadshahryiar@yahoo.com>
2023-03-26 22:27:01 +0500

    User post page

--- client/src/scenes/UserPage.jsx
+++ client/src/scenes/UserPage.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 
 function UserPage() {
     const params = useParams()
+    const navigate = useNavigate()
     const [posts, setUserPosts] = useState([])
 
     const userPosts = async() => {
@@ -30,9 +31,9 @@ function UserPage() {
 
     <>
     
-    <div className='w-full flex justify-center bg-red-100'>
-    <p>UserPage - {params.id}</p>
-    
+    <div className='w-full flex justify-between items-center p-2 bg-red-100'>
+    <p>UserPage - <span className='underline'>{params.id}</span></p>
+    <p className='bg-white p-2 rounded cursor-pointer' onClick={()=>navigate("/index")}>Homepage</p>
 
     </div>
     <div className="w-full bg-green-200 p-2 flex justify-center">
@@ -40,9 +41,14 @@ function UserPage() {
             <div>
               {
                 posts?.map((post)=>(
-                  <p key={post._id} className="p-1 bg-white m-2 h-20">{post.description}</p>
+                  <>
+                  <div key={post._id} className="flex items-center underline p-5 bg-white  m-2 h-20">
+                    <p key={post._id}>{post.description}</p>
+                  </div>
+                  </>
                 ))
               }
+   
             </div>
         </div>
     </div>
